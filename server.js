86b91952f506755dbd78fa82c09fec8e6fa28cc5
Dylan Richards <dylanerichards@gmail.com>
2015-04-22 18:36:28 -0400

    Add Mongoose to project

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 var express = require("express");
+var mongoose = require("mongoose");
 var app = express();
 
 var port = process.env.PORT || 3000;
+
+mongoose.connect("mongodb://localhost:27017/boxes")
 
 var router = express.Router();
 app.use("/", router);
