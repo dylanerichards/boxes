faf0eff83b32ce266d38f0351b5e3b8675043ba5
Dylan Richards <dylanerichards@gmail.com>
2015-04-22 19:01:26 -0400

    Add BodyParser middleware

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 var express = require("express");
 var mongoose = require("mongoose");
+var bodyParser = require("body-parser");
 var Box = require("./models/box");
 var app = express();
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 var port = process.env.PORT || 3000;
 
